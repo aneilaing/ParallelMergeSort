f85809418b97047167fe6d5e92208885d53dd300
aneilaing <aneika.laing@hotmail.com>
2021-08-11 00:52:52 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+using System;
+
+namespace FinalProj_ParallelMergeSort
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.parButton = new System.Windows.Forms.Button();
+            this.seqButton = new System.Windows.Forms.Button();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.viewButton = new System.Windows.Forms.Button();
+            this.namesRichText = new System.Windows.Forms.RichTextBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // parButton
+            // 
+            this.parButton.Font = new System.Drawing.Font("Myanmar Text", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.parButton.Location = new System.Drawing.Point(12, 64);
+            this.parButton.Name = "parButton";
+            this.parButton.Size = new System.Drawing.Size(88, 34);
+            this.parButton.TabIndex = 0;
+            this.parButton.Text = "Parallel";
+            this.parButton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.parButton.UseVisualStyleBackColor = true;
+            this.parButton.Click += new System.EventHandler(this.parButton_Click);
+            // 
+            // seqButton
+            // 
+            this.seqButton.Font = new System.Drawing.Font("Myanmar Text", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seqButton.Location = new System.Drawing.Point(135, 64);
+            this.seqButton.Name = "seqButton";
+            this.seqButton.Size = new System.Drawing.Size(89, 34);
+            this.seqButton.TabIndex = 1;
+            this.seqButton.Text = "Sequential";
+            this.seqButton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.seqButton.UseVisualStyleBackColor = true;
+            this.seqButton.Click += new System.EventHandler(this.seqButton_Click);
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Location = new System.Drawing.Point(79, 158);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(80, 13);
+            this.timerLabel.TabIndex = 2;
+            this.timerLabel.Text = "Execution Time";
+            // 
+            // viewButton
+            // 
+            this.viewButton.Font = new System.Drawing.Font("Myanmar Text", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewButton.Location = new System.Drawing.Point(131, 429);
+            this.viewButton.Name = "viewButton";
+            this.viewButton.Size = new System.Drawing.Size(93, 36);
+            this.viewButton.TabIndex = 3;
+            this.viewButton.Text = "View List";
+            this.viewButton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.viewButton.UseVisualStyleBackColor = true;
+            this.viewButton.Click += new System.EventHandler(this.viewButton_Click);
+            // 
+            // namesRichText
+            // 
+            this.namesRichText.Location = new System.Drawing.Point(82, 199);
+            this.namesRichText.Name = "namesRichText";
+            this.namesRichText.Size = new System.Drawing.Size(205, 212);
+            this.namesRichText.TabIndex = 5;
+            this.namesRichText.Text = "";
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Myanmar Text", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(260, 64);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(93, 36);
+            this.resetButton.TabIndex = 6;
+            this.resetButton.Text = "Reset Sort";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(394, 491);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.namesRichText);
+            this.Controls.Add(this.viewButton);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.seqButton);
+            this.Controls.Add(this.parButton);
+            this.Name = "MainForm";
+            this.Text = "Parallel Merge Sort";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button parButton;
+        private System.Windows.Forms.Button seqButton;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Button viewButton;
+        //private EventHandler nameListBox_SelectedIndexChanged;
+        private System.Windows.Forms.RichTextBox namesRichText;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace FinalProj_ParallelMergeSort
+{
+    public partial class MainForm : Form
+    {
+        private delegate void displayTextboxDelegate(string aname);
+        private bool sorted = false;
+        private string[] nameArray;
+
+        public string[] NamesList   // property
+        {
+            get { 
+                return nameArray;
+            }
+            set { 
+                nameArray = value; 
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void parButton_Click(object sender, EventArgs e)
+        {
+            //create string array with names
+            string[] names = readInNames();
+
+            MergeSort namesList = new MergeSort();
+
+           
+            Stopwatch par_stopwatch = Stopwatch.StartNew();
+
+            namesList.parrallelSort(names, 0, names.Length - 1);
+
+            par_stopwatch.Stop();
+
+            timerLabel.Text = "Parrallel code took " + par_stopwatch.ElapsedMilliseconds +" milliseconds to execute";
+
+            sorted = true;
+            nameArray = names;
+
+
+        }
+
+        private void seqButton_Click(object sender, EventArgs e)
+        {
+
+            //create string array with names
+            string[] names = readInNames();
+
+            MergeSort namesList = new MergeSort();
+
+            Stopwatch seq_stopwatch = Stopwatch.StartNew();
+
+            namesList.sort(names, 0, names.Length - 1);
+
+            seq_stopwatch.Stop();
+
+            timerLabel.Text = "Sequential code took " + seq_stopwatch.ElapsedMilliseconds + " milliseconds to execute";
+
+            sorted = true;
+            nameArray = names;
+        }
+
+        private void viewButton_Click(object sender, EventArgs e)
+        {
+            namesRichText.Clear();
+
+            var thread = new Thread(populateTextbox);
+            thread.IsBackground = true;
+            thread.Start();
+            if (thread.IsAlive == true)
+                viewButton.Enabled = false;
+            else
+                viewButton.Enabled = true;
+
+        }
+
+        private void populateTextbox()
+        {
+            
+
+            if (sorted == true)
+            {
+
+
+                foreach (string aname in nameArray)
+                {
+                    namesRichText.Invoke(new displayTextboxDelegate(displayTextbox), aname);
+
+                }
+
+            }
+
+            else
+            {
+                //create string array with names
+               string[] names = readInNames();
+
+                foreach (string aname in names)
+                {
+                    namesRichText.Invoke(new displayTextboxDelegate(displayTextbox), aname);
+
+                }
+            }
+
+        }
+        private string[] readInNames()
+        {
+            string[] names = System.IO.File.ReadAllLines(@"names.txt");
+            return names;
+        }
+
+        private void displayTextbox(string aname)
+        {
+            namesRichText.AppendText(aname + '\n');
+            
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            string[] names = readInNames();
+            nameArray = names;
+            timerLabel.Text = "Execution Time";
+        }
+    }
+
+    public class MergeSort
+    {
+
+        internal void sort(string[] names, int left, int right)
+        {
+            if (left < right)
+            {
+                //find the middle point of the array in order to split it
+                int middle = (left + right) / 2;
+                //Console.WriteLine("middle is {0}", middle);
+
+                //sort first and second halves
+
+                sort(names, left, middle);
+                sort(names, middle + 1, right);
+
+                //merge the halves
+                merge(names, left, middle, right);
+
+            }
+        }
+
+       internal void parrallelSort(string[] names, int left, int right)
+        {
+            if (left < right)
+            {
+                //find the middle point of the array in order to split it
+                int middle = (left + right) / 2;
+                //Console.WriteLine("middle is {0}", middle);
+
+                //sort first and second halves
+                Parallel.Invoke(() =>
+                {
+                    parrallelSort(names, left, middle);
+                },
+                () =>
+                {
+                    parrallelSort(names, middle + 1, right);
+                }
+                );
+
+                //merge the halves
+                merge(names, left, middle, right);
+
+            }
+        }
+
+        //function that merges the two sorted halves
+        void merge(string[] names, int left, int middle, int right)
+        {
+            //find size of two arrays to be merged
+            int sizeLeft = middle - left + 1;
+            int sizeRight = right - middle;
+
+            //create temporary arrays
+            string[] leftNames = new string[sizeLeft];
+            string[] rightNames = new string[sizeRight];
+
+            //copy data to temporary arrays
+            for (int i = 0; i < sizeLeft; ++i)
+                leftNames[i] = names[left + i];
+            for (int j = 0; j < sizeRight; ++j)
+                rightNames[j] = names[middle + 1 + j];
+
+            //merge the temporary arrays
+
+            //create starting indexes for the two arrays
+            int iterator = 0;
+            int jterator = 0;
+
+            int k = left;
+            while (iterator < sizeLeft && jterator < sizeRight)
+            {
+                string[] leftFullName = leftNames[iterator].Split(' ');
+                string[] rightFullName = rightNames[jterator].Split(' ');
+                //Console.WriteLine("Left last name {0}", leftFullName[1]);
+
+                if (String.Compare(leftFullName[1].ToUpper(), rightFullName[1].ToUpper()) == 0)
+                {
+                    if (String.Compare(leftFullName[0].ToUpper(), rightFullName[0].ToUpper()) < 0)
+                    {
+                        names[k] = leftNames[iterator];
+                        iterator++;
+                    }
+                    else
+                    {
+                        names[k] = rightNames[jterator];
+                        jterator++;
+                    }
+                }
+                else
+                {
+
+                    if (String.Compare(leftFullName[1].ToUpper(), rightFullName[1].ToUpper()) < 0)
+                    {
+                        names[k] = leftNames[iterator];
+                        iterator++;
+                    }
+                    else
+                    {
+                        names[k] = rightNames[jterator];
+                        jterator++;
+                    }
+                }
+                k++;
+            }
+
+            //Copy remaining elements of the left array, if any
+            while (iterator < sizeLeft)
+            {
+                names[k] = leftNames[iterator];
+                iterator++;
+                k++;
+            }
+
+            //Copy remaining elements of the right array, if any
+            while (jterator < sizeRight)
+            {
+                names[k] = rightNames[jterator];
+                jterator++;
+                k++;
+            }
+
+
+        }
+
+        static void printNames(string[] names)
+        {
+            int n = names.Length;
+            for (int i = 0; i < n; ++i)
+                Console.WriteLine(names[i]);
+            Console.WriteLine();
+        }
+
+        
+    }
+}
